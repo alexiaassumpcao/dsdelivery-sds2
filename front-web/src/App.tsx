424d37baa42b60271a094d5ee51e0dfbd6241b97
Alexia <alexiaassumpcaorp@gmail.com>
2021-01-09 12:59:07 -0300

    Adiciona Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import Hello from './Hello'
-import Counter from './Counter'
-;
+import Navbar from './Navbar'
+
 function App() {
   return (
     <div>
-     <h1>Hello!</h1>
+     <Navbar/>
     </div>
 
   );
