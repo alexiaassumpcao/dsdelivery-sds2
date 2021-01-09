db493a1750b52f29f93dd178cc02757264765333
Alexia <alexiaassumpcaorp@gmail.com>
2021-01-09 19:43:28 -0300

    Permite salvar pedido

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
 const API_URL = 'http://localhost:8080'
 const MAPBOX_TOKEN = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX
@@ -10,4 +11,8 @@ export function fetchProducts(){
 
 export function fetchLocalMapBox(local: string){
     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${MAPBOX_TOKEN}`)
+}
+
+export function saveOrder(order: OrderPayload){
+    return axios.post(`${API_URL}/orders`, order)
 }
